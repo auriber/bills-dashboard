1357cdf34e60e46319dcddceb2e07bf88fcd14e7
aurimas.berskys <aurimas.berskys@tesonet.com>
2020-01-20 22:30:11 +0200

    Add Bill repository class

---
+++ src/Repository/BillRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Bill;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method Bill|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Bill|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Bill[]    findAll()
+ * @method Bill[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class BillRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Bill::class);
+    }
+}
